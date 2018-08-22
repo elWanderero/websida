7d9bb18a662bd93b2aaab102590d0e4714cb693c
Daniel <danielzs@kth.se>
2018-08-22 21:55:57 +0200

    Forgot to actually remove credentials... Fixed now

--- www-root/php/DBconnect.php
+++ www-root/php/DBconnect.php
@@ -1,9 +1,10 @@
 <?php
+// Template file, keep actual credentials only on server, not in development.
 // Initiering av databas, till för att includas i annan PHP.
-define('DB_SERVER', 'localhost');
-define('DB_USERNAME', 'root');
-define('DB_PASSW', '');
-define('DB_NAME', 'daniels_blogg');
+define('DB_SERVER', 'server URL');
+define('DB_USERNAME', 'username');
+define('DB_PASSW', 'password');
+define('DB_NAME', 'database name');
 
 $db_connection = new mysqli(DB_SERVER, DB_USERNAME, DB_PASSW, DB_NAME);
 if ($db_connection->connect_error) {die("Connection failed: " . $db_connection->connect_error);}
